09d93ad0b42d3f52696c0a3cbb079103dc848cbd
Ronald Fernando Gómez Trejo <ronaldfernandotrejo@gmail.com>
2019-06-16 16:24:33 -0600

    Bola
    Agregando esfera y comportamientos básicos

---
+++ Futbolin/Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    Rigidbody rb;
+    public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate()
+    {
+        float posicionHorizontal = Input.GetAxis("Horizontal") * velocidad;
+        float posicionVertical = Input.GetAxis("Vertical") * velocidad;
+
+        Vector3 movimiento = new Vector3(posicionHorizontal, 0, posicionVertical);
+
+        rb.AddForce(movimiento, ForceMode.Force);
+    }
+}
